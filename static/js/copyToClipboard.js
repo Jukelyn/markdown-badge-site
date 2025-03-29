ba9af2aff862b6c1b87b3b6f1eac9bfc8c26b6e8
Jukelyn <viperkillergaming@gmail.com>
2025-03-29 15:30:22 -0400

    Added popup modal when copying to clipboard

--- static/js/copyToClipboard.js
+++ static/js/copyToClipboard.js
@@ -1,12 +1,30 @@
 function copyToClipboard() {
-  navigator.clipboard
-    .writeText(selectedBadgesTextarea.value)
-    .then(() => {
-      console.log("Content copied to clipboard!");
-    })
-    .catch((error) => {
-      alert("Failed to copy content: " + error);
-    });
+  const selectedBadgesTextarea = document.getElementById("selectedBadges");
+  if (selectedBadgesTextarea.value) {
+    navigator.clipboard
+      .writeText(selectedBadgesTextarea.value)
+      .then(() => {
+        console.log("Content copied to clipboard!");
+        showModal();
+      })
+      .catch((error) => {
+        alert("Failed to copy content: " + error);
+      });
+  }
+}
+
+function showModal() {
+  const modal = document.getElementById("popupModal");
+  modal.style.display = "block";
+
+  const closeModalButton = document.getElementById("closeModal");
+  closeModalButton.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  setTimeout(() => {
+    modal.style.display = "none";
+  }, 3000);
 }
 
 const copyButton = document.getElementById("copyButton");
