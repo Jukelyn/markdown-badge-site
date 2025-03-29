03472957a34eed9c2e334b94ac055f65f6a354ab
Jukelyn <viperkillergaming@gmail.com>
2025-03-29 01:35:17 -0400

    Added table for badges and ability to select badges and output them to a textarea

--- src/routes/index.py
+++ src/routes/index.py
@@ -1,7 +1,10 @@
 # pylint: disable=E0401
 """This module provides the route for the index page."""
+import json
 from flask import render_template
 from src.utils import timestamp
+
+DATA_PATH = "data/badges.json"
 
 
 def index_route(app):
@@ -18,5 +21,11 @@ def index_route(app):
         Handles requests to the main `/` route.
         """
 
+        with open(DATA_PATH, "r", encoding="utf-8") as f:
+            data = json.load(f)
+
+        print(data)
         current_time = timestamp.get_time()
-        return render_template("index.html", currentTime=current_time)
+        return render_template("index.html",
+                               data=data,
+                               currentTime=current_time)
