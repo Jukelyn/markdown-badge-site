a5e4ab0396a0a9b75c4ae3025c2aa42f3fbdc559
Jukelyn <viperkillergaming@gmail.com>
2025-03-30 03:51:48 -0400

    eslint go away

--- static/ts/clearTextarea.ts
+++ static/ts/clearTextarea.ts
@@ -1,3 +1,4 @@
+/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 function clearTextarea(): void {
   const textarea = document.getElementById(
     "customizedBadges"
--- static/ts/copyToClipboard.ts
+++ static/ts/copyToClipboard.ts
@@ -1,3 +1,4 @@
+/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 function copyToClipboard(targetTextareaId: string): void {
   const textarea = document.getElementById(targetTextareaId) as HTMLTextAreaElement;
   if (textarea && textarea.value) {
