7ed33fced73301491c5324e4278a4def885cd311
Jukelyn <viperkillergaming@gmail.com>
2025-03-30 03:38:28 -0400

    Converted JS to TS

--- static/js/getBadges.js
+++ static/js/getBadges.js
@@ -1,46 +1,40 @@
+"use strict";
 const selectedBadges = []; // Array to store selected badges
 const tbodyEl = document.querySelector("#selectedBadgesTable tbody");
 const selectedBadgesTextarea = document.querySelector("#selectedBadges");
-
 // Function to handle checkbox click and update the selected badges
 function toggleCheckbox(row) {
-  const checkbox = row.querySelector('input[type="checkbox"]');
-  const badgeName = row.getAttribute("data-badge-name");
-  const badgeURL = row.getAttribute("data-badge-url");
-  const rowIndex = Array.from(row.parentNode.children).indexOf(row); // Get the index of the row
-
-  if (checkbox.checked) {
-    // Add the badge to the array (preserve row index for order)
-    selectedBadges.push({ badgeName, badgeURL, rowIndex });
-  } else {
-    // Remove the badge from the array if unchecked
-    const index = selectedBadges.findIndex(
-      (item) => item.badgeURL === badgeURL
-    );
-    if (index !== -1) {
-      selectedBadges.splice(index, 1);
+    const checkbox = row.querySelector('input[type="checkbox"]');
+    const badgeName = row.getAttribute("data-badge-name");
+    const badgeURL = row.getAttribute("data-badge-url");
+    // Get the index of the row
+    const rowIndex = Array.from(row.parentNode.children).indexOf(row);
+    if (checkbox.checked) {
+        // Add the badge to the array (preserve row index for order)
+        selectedBadges.push({ badgeName, badgeURL, rowIndex });
     }
-  }
-
-  // Sort the selectedBadges array by the row index to maintain the order
-  selectedBadges.sort((a, b) => a.rowIndex - b.rowIndex);
-
-  // Update content
-  updateContent();
+    else {
+        // Remove the badge from the array if unchecked
+        const index = selectedBadges.findIndex((item) => item.badgeURL === badgeURL);
+        if (index !== -1) {
+            selectedBadges.splice(index, 1);
+        }
+    }
+    // Sort the selectedBadges array by the row index to maintain the order
+    selectedBadges.sort((a, b) => a.rowIndex - b.rowIndex);
+    // Update content
+    updateContent();
 }
-
 // Update textarea and selected badges table with selected badges
 function updateContent() {
-  // Update the textarea with selected badge URLs in order of selection
-  selectedBadgesTextarea.value = selectedBadges
-    .map((item) => `![${item.badgeName}](${item.badgeURL})`)
-    .join("\n");
-
-  // Update the selected badges table
-  tbodyEl.innerHTML = ""; // Clear existing rows
-
-  selectedBadges.forEach((item) => {
-    const row = `
+    // Update the textarea with selected badge URLs in order of selection
+    selectedBadgesTextarea.value = selectedBadges
+        .map((item) => `![${item.badgeName}](${item.badgeURL})`)
+        .join("\n");
+    // Update the selected badges table
+    tbodyEl.innerHTML = ""; // Clear existing rows
+    selectedBadges.forEach((item) => {
+        const row = `
       <tr>
         <td>
           <label class="container">
@@ -53,14 +47,14 @@ function updateContent() {
         <td><a href="${item.badgeURL}" target="_blank">${item.badgeURL}</a></td>
       </tr>
     `;
-    tbodyEl.innerHTML += row;
-  });
+        tbodyEl.innerHTML += row;
+    });
 }
-
 const badgeTable = document.getElementById("badgeTable");
 badgeTable.addEventListener("click", function (e) {
-  if (e.target.type === "checkbox") {
-    const row = e.target.closest("tr");
-    toggleCheckbox(row);
-  }
+    const target = e.target;
+    if (target instanceof HTMLInputElement && target.type === "checkbox") {
+        const row = target.closest("tr");
+        toggleCheckbox(row);
+    }
 });
